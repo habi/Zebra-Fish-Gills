0524e6b32c99231eff7d03c781ad395438dc04a8
David Haberthür <email@davidhaberthuer.ch>
2019-08-06 12:20:11 +0200

    Add download script for all data on OSF
    Workaround until https://github.com/osfclient/osfclient/issues/169 is fixed

---
+++ download.osf.sh
@@ -0,0 +1,26 @@
+# Controls
+wget -nc -O c01.zip https://osf.io/7utfb/download
+wget -nc -O c02.zip https://osf.io/yg4xr/download
+wget -nc -O c03.zip https://osf.io/9awx4/download
+wget -nc -O c04.zip https://osf.io/57xbk/download
+wget -nc -O c05.zip https://osf.io/8kucf/download
+wget -nc -O c06.zip https://osf.io/ntxua/download
+wget -nc -O c07.zip https://osf.io/bhqez/download
+wget -nc -O c08.zip https://osf.io/xwu97/download
+wget -nc -O c09.zip https://osf.io/qp584/download
+wget -nc -O c10.zip https://osf.io/d3hf5/download
+
+# Swimmers
+wget -nc -O s01.zip https://osf.io/2jg5h/download
+wget -nc -O s02.zip https://osf.io/5be6p/download
+wget -nc -O s03.zip https://osf.io/j8mvb/download
+wget -nc -O s04.zip https://osf.io/h4eyz/download
+wget -nc -O s05.zip https://osf.io/9fzqj/download
+wget -nc -O s06.zip https://osf.io/rcmsj/download
+wget -nc -O s07.zip https://osf.io/av7pd/download
+wget -nc -O s08.zip https://osf.io/xgq2u/download
+wget -nc -O s09.zip https://osf.io/8w3gu/download
+wget -nc -O s10.zip https://osf.io/yp23e/download
+
+# Unzip all the files
+for i in *.zip; do unzip $i; done
